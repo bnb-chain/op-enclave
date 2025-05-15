4a8b727a826eb3eea075dedabfe8da50a67bcb9b
VM <arimas@foxmail.com>
2025-05-15 21:47:43 +0800

    feat: add L2GenesisDeploy script

---
+++ opbnb-contracts/script/L2GenesisDeploy.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {L2Genesis} from "@opbnb-bedrock/scripts/L2Genesis.s.sol";
+import {Deployer} from "@opbnb-bedrock/scripts/Deployer.sol";
+import {console2 as console} from "forge-std/console2.sol";
+
+contract L2GenesisDeploy is L2Genesis, Deployer {
+    function generateForkAllocs() public {
+        console.log("Generate  fork allocs!");
+        runWithAllUpgrades();
+    }
+}
