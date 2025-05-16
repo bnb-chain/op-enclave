81315d373b8d1424ef1277d49590802664836b5e
VM <arimas@foxmail.com>
2025-05-16 18:24:15 +0800

    fix: update dir read permission

--- opbnb-contracts/script/L2GenesisDeploy.s.sol
+++ opbnb-contracts/script/L2GenesisDeploy.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import {L2Genesis} from "@opbnb-bedrock/scripts/L2Genesis.s.sol";
+import {L2Genesis, OutputMode} from "@opbnb-bedrock/scripts/L2Genesis.s.sol";
 import {Deployer} from "@opbnb-bedrock/scripts/Deployer.sol";
 import {console2 as console} from "forge-std/console2.sol";
 
@@ -9,5 +9,6 @@ contract L2GenesisDeploy is L2Genesis {
     function generateForkAllocs() public {
         console.log("Generate fork allocs!");
         runWithAllUpgrades();
+        // runWithOptions(OutputMode.OUTPUT_ALL, artifactDependencies());
     }
 }
